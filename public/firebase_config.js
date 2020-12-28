ad5ab61cfadc57cc62cb8df51e9359f8880db3f8
agilan2001 <agilanvlr2001@gmail.com>
2020-12-28 17:23:01 +0530

    add firebase-cofig

---
+++ public/firebase_config.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyAz7ntyy2tUbzUK5g9kADwOnbceejh6CsU",
+    authDomain: "share-kural.firebaseapp.com",
+    databaseURL: "https://share-kural-default-rtdb.firebaseio.com",
+    projectId: "share-kural",
+    storageBucket: "share-kural.appspot.com",
+    messagingSenderId: "320843254946",
+    appId: "1:320843254946:web:c0dc6e066899e93e4cdbfb",
+    measurementId: "G-04YNCNY0S1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
